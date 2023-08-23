abf51fd96fffb39b0dfc926f9345aaea5a765fb2
Viral Savaliya <vdsavaliya95@gmail.com>
2023-08-23 12:03:57 +0530

    Initial commit

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Viral Savaliya on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
